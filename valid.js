194fd4ab7816cca692d51696bc488f29e7731c46
Gokul-Gokzzz <gokulunniu88@gmail.com>
2024-01-27 12:49:45 +0530

    changes made

--- valid.js
+++ valid.js
@@ -1,22 +1,51 @@
-// function myfunction(){
-//     var x =document.getElementById("pass");
-    
-//     if(x.type ==="password"){
-//         x.type ="text";
-//     }else{
-//         x.type = "password";
-//     }
-// }
-
-function validate() {
-    var username = document.getElementById("username").value;
+var username = document.getElementById("username").value;
     var password = document.getElementById("pass").value;
 
-    if (username.length > 0 && password.length >= 8) {
-        alert("Login successful");
-        return true; 
-    } else {
-        alert("Login failed. Please provide a valid username and password.");
-        return false; 
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+function validate() {
+    var expectedEmail = "gokulunniu88@gmail.com";
+    var expectedPassword = "Gokz2255";
+
+    
+    if (email.value !== expectedEmail) {
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
     }
+    if (password.value !== expectedPassword) {
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+
+    return true;
+}
+
+
+function email_Verify() {
+    if (email.value.length >= 8) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+
+function pass_Verify() {
+    if (password.value.length >= 5) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+}
+
+function logout() {
+ 
+    window.location.href = "login.html";
 }
